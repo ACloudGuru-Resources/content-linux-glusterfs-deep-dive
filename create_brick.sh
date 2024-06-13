8867f02f91bc9bd2fc9b917542fffdd66a51e664
Marcos Monteiro <marcos-monteiro@pluralsight.com>
2024-06-13 11:32:34 +1000

    Update script to detect block device dynamically

--- create_brick.sh
+++ create_brick.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#Partition disk
-/usr/sbin/parted --script -a optimal -- /dev/nvme1n1 mklabel gpt mkpart primary 1MiB -1
+# Detect the block device dynamically
+block_device=$(lsblk -dpno NAME,TYPE | awk '$2=="disk" && $1!~/nvme[0-9]n1$/ {print $1}')
+
+# Partition disk
+/usr/sbin/parted --script -a optimal -- ${block_device} mklabel gpt mkpart primary 1MiB -1
 sleep 2
 
-#Create Filesystem
-mkfs.xfs -i size=512 /dev/nvme1n1p1
+# Create Filesystem
+mkfs.xfs -i size=512 "${block_device}p1"
 
-#add to /etc/fstab, mount filesystem, and create brick subdirectory
-UUID=`blkid /dev/nvme1n1p1 | cut -d \" -f 2`
+# Add to /etc/fstab, mount filesystem, and create brick subdirectory
+UUID=$(blkid "${block_device}p1" | cut -d \" -f 2)
 echo "UUID=$UUID /gfs xfs rw,inode64,noatime,nouuid 1 2" >> /etc/fstab
 mkdir /gfs
 mount /gfs
