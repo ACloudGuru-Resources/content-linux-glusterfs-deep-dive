ff64c9a612bd92a9e77c5b409f94d590ed89250e
Marcos Monteiro <marcos-monteiro@pluralsight.com>
2024-06-13 12:59:05 +1000

    Fix command to detect free block device dynamically

--- create_brick.sh
+++ create_brick.sh
@@ -5,14 +5,14 @@ busy=$(lsblk | grep part | awk '{ print $1 }' | head -1 | cut -b 7-13)
 free=$(lsblk | grep -v ${busy} | grep -v NAME | awk '{ print $1 }')
 
 # Partition disk
-/usr/sbin/parted --script -a optimal -- ${free} mklabel gpt mkpart primary 1MiB -1
+/usr/sbin/parted --script -a optimal -- /dev/${free} mklabel gpt mkpart primary 1MiB -1
 sleep 2
 
 # Create Filesystem
-mkfs.xfs -i size=512 "${free}p1"
+mkfs.xfs -i size=512 /dev/${free}p1
 
 # Add to /etc/fstab, mount filesystem, and create brick subdirectory
-UUID=$(blkid "${free}p1" | cut -d \" -f 2)
+UUID=$(blkid /dev/${free}p1 | cut -d \" -f 2)
 echo "UUID=$UUID /gfs xfs rw,inode64,noatime,nouuid 1 2" >> /etc/fstab
 mkdir /gfs
 mount /gfs
